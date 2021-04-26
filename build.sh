54bdd05345e73b7f0c1814b39e891fef49f6a33b
AngeloMateus <angelomateus.music@gmail.com>
2021-04-26 23:53:52 +0100

    added release binary

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 if [[ "$1" == '--debug' ]]; then
 cmake -S . -B build/debug -DCMAKE_BUILD_TYPE=Debug; cmake --build build/debug
 exit 0
+elif [[ "$1" == '--release' ]]; then
+cmake -S . -B build/release -DCMAKE_BUILD_TYPE=Release; cmake --build build/release
+exit 0
 else
 echo "No option"
 exit 1
